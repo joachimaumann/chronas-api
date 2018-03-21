78d290072a41f0d8bfe1471ae1ca9c388d586252
Joachim Aumann <joachimaumann@Joachims-Air.fritz.box>
2018-03-21 20:00:50 +0100

    updated commands

--- kuberneties/deployTok8s.sh
+++ kuberneties/deployTok8s.sh
@@ -2,7 +2,9 @@
 
 #kubectl create -f mongo-deployment.yml
 #kubectl create -f mongo-service.yml
-kubectl apply -f chronas-secret.yml
+kubectl delete secret chronas-secrets
+#kubectl create secret generic chronas-secrets --from-file=$(System.DefaultWorkingDirectory)/secrets
+kubectl create -f chronas-secret.yml
 kubectl apply -f chronas-map.yml
 kubectl apply -f chronas-front-deployment.yml
 kubectl apply -f chronas-front-service.yml
